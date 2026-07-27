2bc6294897408faabafa127e4a03d4ecf6b5c879
Dylan Donnell <dylan.donnell@student.griffith.ie>
2026-07-27 21:07:34 +0100

    feat: add alpine and freebsd CI

--- src/ipc_subscribe.c
+++ src/ipc_subscribe.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
 static subscriber_t *make_subscriber(int client_fd, char *fifo_path, subscriber_mask_t mask,
 		int count) {
